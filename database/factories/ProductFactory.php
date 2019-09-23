a411f8c7574840b71ac7770f09b18b8f44ff1c9c
Kesi Promnual <kesi.promnual@myzone.org>
2019-09-23 11:55:08 +0100

    Seeding Fake data to product and review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,8 +5,13 @@
 use App\Model\Product;
 use Faker\Generator as Faker;
 
+//Faker is a PHP library that generates fake data for you
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,50)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -5,8 +5,14 @@
 use App\Model\Review;
 use Faker\Generator as Faker;
 
+//Faker is a PHP library that generates fake data for you then we need to seed it
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+          return App\Model\Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' =>$faker->paragraph,
+        'star' =>$faker-> numberBetween(0,5)
     ];
 });
